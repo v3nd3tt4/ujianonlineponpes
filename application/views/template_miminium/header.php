dce48b75b8eabb80de37b43867bd2501b3eae151
Okta Pilopa <pilopaokta@gmail.com>
2025-06-20 09:14:44 +0800

    add template

---
+++ application/views/template_miminium/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<meta charset="utf-8">
+	<meta name="description" content="Miminium Admin Template v.1">
+	<meta name="author" content="Isna Nur Azis">
+	<meta name="keyword" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Miminium</title>
+ 
+    <!-- start: Css -->
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets_miminium/css/bootstrap.min.css">
+
+      <!-- plugins -->
+      <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets_miminium/css/plugins/font-awesome.min.css"/>
+      <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets_miminium/css/plugins/simple-line-icons.css"/>
+      <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets_miminium/css/plugins/animate.min.css"/>
+      <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets_miminium/css/plugins/fullcalendar.min.css"/>
+	<link href="<?=base_url()?>assets_miminium/css/style.css" rel="stylesheet">
+	<!-- end: Css -->
+
+	<link rel="shortcut icon" href="<?=base_url()?>assets_miminium/img/logomi.png">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
