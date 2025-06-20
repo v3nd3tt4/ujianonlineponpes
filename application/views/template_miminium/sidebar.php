c3909cec18ba0c6a3f08c63c486383babace8327
Okta Pilopa <pilopaokta@gmail.com>
2025-06-20 15:37:15 +0800

    remove clock

--- application/views/template_miminium/sidebar.php
+++ application/views/template_miminium/sidebar.php
@@ -9,10 +9,10 @@
         <div class="sub-left-menu scroll">
           <ul class="nav nav-list">
               <li><div class="left-bg"></div></li>
-              <li class="time">
+              <!-- <li class="time">
                 <h1 class="animated fadeInLeft">21:00</h1>
                 <p class="animated fadeInRight">Sat,October 1st 2029</p>
-              </li>
+              </li> -->
               <!-- <li class="active ripple">
                 <a class="tree-toggle nav-header"><span class="fa-home fa"></span> Dashboard 
                   <span class="fa-angle-right fa right-arrow text-right"></span>
