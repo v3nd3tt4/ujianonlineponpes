9f783b74828cd8f50fa95d71aded2a877bfb1a59
mac <mac@macs-MacBook-Air.local>
2025-07-19 22:13:51 +0800

    add db terbaru

--- db_ujianonlineponpes (1).sql
+++ db_ujianonlineponpes (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 19, 2025 at 01:06 PM
+-- Generation Time: Jul 19, 2025 at 04:13 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -39,11 +39,20 @@ CREATE TABLE `ci_sessions` (
 --
 
 INSERT INTO `ci_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES
+('101f123e561dc7e5273f7ed63b6747c0c219d728', '::1', 1752933221, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323933333232313b69645f757365727c733a313a2231223b6e616d617c733a353a2241646d696e223b72756c657c733a353a2261646d696e223b6c6f676765645f696e7c623a313b),
 ('1600eeb06f6240cd8746f4b038d2e8f4af013b60', '::1', 1752921514, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323932313531343b69645f757365727c733a313a2234223b6e616d617c733a343a2247757275223b72756c657c733a343a2267757275223b6c6f676765645f696e7c623a313b),
 ('212f6fe56d99ef1546fc63077f1bb70973801d1b', '::1', 1752922817, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323932323831373b69645f757365727c733a313a2234223b6e616d617c733a343a2247757275223b72756c657c733a343a2267757275223b6c6f676765645f696e7c623a313b),
 ('284b5aaf1fbb8e52d49122da0fcf6ec4a15b3b11', '::1', 1752923123, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323932333132333b69645f757365727c733a313a2234223b6e616d617c733a343a2247757275223b72756c657c733a343a2267757275223b6c6f676765645f696e7c623a313b),
-('736c6708b9dd6d7505e11320fb1951ee7362aa34', '::1', 1752923123, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323932333132333b69645f757365727c733a313a2234223b6e616d617c733a343a2247757275223b72756c657c733a343a2267757275223b6c6f676765645f696e7c623a313b),
-('758cf43af0101f35565b6e1168d6ab64314ab4f1', '::1', 1752918239, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323931383233393b6572726f727c733a33333a22456d61696c2c2070617373776f72642c206174617520726f6c652073616c616821223b5f5f63695f766172737c613a313a7b733a353a226572726f72223b733a333a226f6c64223b7d);
+('45c376e3c7f09c81d471369a808be283a3a24b69', '::1', 1752923928, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323932333932383b69645f757365727c733a313a2234223b6e616d617c733a343a2247757275223b72756c657c733a343a2267757275223b6c6f676765645f696e7c623a313b737563636573737c733a33323a22446174612070726f66696c20626572686173696c206469706572626172756921223b5f5f63695f766172737c613a313a7b733a373a2273756363657373223b733a333a226f6c64223b7d),
+('736c6708b9dd6d7505e11320fb1951ee7362aa34', '::1', 1752923586, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323932333538363b69645f757365727c733a313a2234223b6e616d617c733a343a2247757275223b72756c657c733a343a2267757275223b6c6f676765645f696e7c623a313b),
+('758cf43af0101f35565b6e1168d6ab64314ab4f1', '::1', 1752918239, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323931383233393b6572726f727c733a33333a22456d61696c2c2070617373776f72642c206174617520726f6c652073616c616821223b5f5f63695f766172737c613a313a7b733a353a226572726f72223b733a333a226f6c64223b7d),
+('b12f88b2af7f8c9d776e56be4ea5cef9b9fad260', '::1', 1752932803, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323933323830333b69645f757365727c733a313a2231223b6e616d617c733a353a2241646d696e223b72756c657c733a353a2261646d696e223b6c6f676765645f696e7c623a313b),
+('c6456c9d3ab56265868e114f8d8585f8d144d3c7', '::1', 1752934248, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323933343234383b69645f757365727c733a313a2232223b6e616d617c733a31343a224b6570616c612053656b6f6c6168223b72756c657c733a31343a226b6570616c612073656b6f6c6168223b6c6f676765645f696e7c623a313b),
+('c67472d086f18caa65cd0066f911d446f4994fda', '::1', 1752933626, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323933333632363b69645f757365727c733a313a2231223b6e616d617c733a353a2241646d696e223b72756c657c733a353a2261646d696e223b6c6f676765645f696e7c623a313b),
+('d369b55b5352b38cf1c9623829b5c8e06dd61643', '::1', 1752932286, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323933323238363b69645f757365727c733a313a2232223b6e616d617c733a343a2244696168223b72756c657c733a353a227369737761223b6c6f676765645f696e7c623a313b6e69737c733a393a22313233313233313233223b),
+('ef5837528b6c762334a392b69427243eaba3f737', '::1', 1752924635, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323932343633353b69645f757365727c733a313a2231223b6e616d617c733a353a22416e676769223b72756c657c733a353a227369737761223b6c6f676765645f696e7c623a313b6e69737c733a363a22313233343536223b),
+('ef80af5f6db72ee123c007c6a83174e7266a428b', '::1', 1752924999, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323932343939393b69645f757365727c733a313a2231223b6e616d617c733a353a22416e676769223b72756c657c733a353a227369737761223b6c6f676765645f696e7c623a313b6e69737c733a363a22313233343536223b),
+('f16acb3b6ccebb8b178fa73570580ce421c2c5c5', '::1', 1752934248, 0x5f5f63695f6c6173745f726567656e65726174657c693a313735323933343234383b69645f757365727c733a313a2232223b6e616d617c733a31343a224b6570616c612053656b6f6c6168223b72756c657c733a31343a226b6570616c612073656b6f6c6168223b6c6f676765645f696e7c623a313b);
 
 -- --------------------------------------------------------
 
@@ -225,12 +234,14 @@ INSERT INTO `tb_matapelajaran` (`id`, `kode_matapelajaran`, `nama_matapelajaran`
 
 CREATE TABLE `tb_pegawai` (
   `id` int(11) NOT NULL,
+  `nik` varchar(30) DEFAULT NULL,
   `nama` varchar(100) NOT NULL,
   `tempat_lahir` varchar(100) NOT NULL,
   `tanggal_lahir` date NOT NULL,
   `jenis_kelamin` enum('L','P') NOT NULL,
   `alamat` text NOT NULL,
   `no_telepon` varchar(20) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT 'default.jpg',
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('admin','operator','guru','kepala sekolah') NOT NULL,
@@ -242,11 +253,12 @@ CREATE TABLE `tb_pegawai` (
 -- Dumping data for table `tb_pegawai`
 --
 
-INSERT INTO `tb_pegawai` (`id`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `no_telepon`, `email`, `password`, `role`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'Bandar Lampung', '2025-06-25', 'L', '-', '0', 'admin@mail.com', '$2y$10$r0pZbKup.H8dsl04boCcCeGW5QsTaoUZFT98XSNJuPthlQR7tqSI.', 'admin', '2025-06-24 01:25:28', '2025-06-27 15:13:06'),
-(2, 'Kepala Sekolah', '-', '2000-06-04', 'L', '-', '0', 'kepsek@mail.com', '$2y$10$fUxDpXjIzbjR.JQ2noL0Hepw95ENUe/aUJd.ayZmgKVGPkBMoGIhG', 'kepala sekolah', '2025-06-20 02:44:07', '2025-06-27 04:11:44'),
-(3, 'Operator', '-', '1998-10-26', 'L', '-', '0', 'operator@mail.com', '$2y$10$IGkfI/qGUunBeorfvIwLou2gYxY9cjxeWA47RLMlpFjuMwH5KV6lG', 'operator', '2025-06-20 06:33:09', '2025-06-27 04:11:50'),
-(4, 'Guru', '-', '2025-06-27', 'L', '-', '0', 'guru@mail.com', '$2y$10$W2cstmP1DaSKWBi9Tu.CNuRQ.ZuihBGdHppUSU6Xdv.4RXUzo4AyS', 'guru', '2025-06-27 04:11:36', '2025-06-27 04:12:07');
+INSERT INTO `tb_pegawai` (`id`, `nik`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `no_telepon`, `foto`, `email`, `password`, `role`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'Admin', 'Bandar Lampung', '2025-06-25', 'L', '-', '0', 'default.jpg', 'admin@mail.com', '$2y$10$r0pZbKup.H8dsl04boCcCeGW5QsTaoUZFT98XSNJuPthlQR7tqSI.', 'admin', '2025-06-24 01:25:28', '2025-06-27 15:13:06'),
+(2, NULL, 'Kepala Sekolah', '-', '2000-06-04', 'L', '-', '0', 'default.jpg', 'kepsek@mail.com', '$2y$10$fUxDpXjIzbjR.JQ2noL0Hepw95ENUe/aUJd.ayZmgKVGPkBMoGIhG', 'kepala sekolah', '2025-06-20 02:44:07', '2025-06-27 04:11:44'),
+(3, NULL, 'Operator', '-', '1998-10-26', 'L', '-', '0', 'default.jpg', 'operator@mail.com', '$2y$10$IGkfI/qGUunBeorfvIwLou2gYxY9cjxeWA47RLMlpFjuMwH5KV6lG', 'operator', '2025-06-20 06:33:09', '2025-06-27 04:11:50'),
+(4, NULL, 'Guru', '-', '2025-06-27', 'P', '-hhh', '0', 'profile_4_1752924144.jpg', 'guru@mail.com', '$2y$10$W2cstmP1DaSKWBi9Tu.CNuRQ.ZuihBGdHppUSU6Xdv.4RXUzo4AyS', 'guru', '2025-06-27 04:11:36', '2025-07-19 10:22:24'),
+(5, 'Et laboris incididunt sed temp', 'Anim explicabo Sed ', 'Voluptas sed aliquip', '2001-08-08', 'P', 'Ex optio libero atq', 'Et soluta elit aut ', 'default.jpg', 'hycigyx@mailinator.com', '$2y$10$jAgpddg8kwECiUND/IyMKuGOV3j9DjDjL7dKuy2Nex7nvyBslmJsi', 'guru', '2025-07-19 13:46:49', '2025-07-19 13:46:49');
 
 -- --------------------------------------------------------
 
@@ -299,6 +311,7 @@ CREATE TABLE `tb_siswa` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `no_hp` varchar(20) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT 'default.jpg',
   `nama_ibu` varchar(100) DEFAULT NULL,
   `nama_ayah` varchar(100) DEFAULT NULL,
   `pekerjaan_ibu` varchar(100) DEFAULT NULL,
@@ -310,9 +323,9 @@ CREATE TABLE `tb_siswa` (
 -- Dumping data for table `tb_siswa`
 --
 
-INSERT INTO `tb_siswa` (`id`, `nama`, `nis`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `email`, `password`, `no_hp`, `nama_ibu`, `nama_ayah`, `pekerjaan_ibu`, `pekerjaan_ayah`, `tahun_masuk`) VALUES
-(1, 'Anggi', '123456', '-', '2025-06-27', 'P', '-', 'anggi@mail.com', '$2y$10$eM08LLxL8nhoHyNlVqb2gejekbXYiSyzKVJZjfHfiHIxIodisYsLe', '0', '-', '-', '-', '-', '2022'),
-(2, 'Diah', '123123123', '-', '2025-06-27', 'P', '-', 'diah@mail.com', '$2y$10$jgyq3zcDxgMIg580jTzNCuNq0Q7r6dSOvd1ZfQtlVqfjBQQeKMH1e', '0', '-', '-', '-', '-', '2023');
+INSERT INTO `tb_siswa` (`id`, `nama`, `nis`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `email`, `password`, `no_hp`, `foto`, `nama_ibu`, `nama_ayah`, `pekerjaan_ibu`, `pekerjaan_ayah`, `tahun_masuk`) VALUES
+(1, 'Anggi', '123456', '-', '2025-06-27', 'P', '-', 'anggi@mail.com', '$2y$10$eM08LLxL8nhoHyNlVqb2gejekbXYiSyzKVJZjfHfiHIxIodisYsLe', '0', 'profile_siswa_1_1752924648.jpg', '-', '-', '-', '-', '2022'),
+(2, 'Diah', '123123123', '-', '2025-06-27', 'P', '-', 'diah@mail.com', '$2y$10$jgyq3zcDxgMIg580jTzNCuNq0Q7r6dSOvd1ZfQtlVqfjBQQeKMH1e', '0', 'default.jpg', '-', '-', '-', '-', '2023');
 
 -- --------------------------------------------------------
 
@@ -433,7 +446,8 @@ ALTER TABLE `tb_matapelajaran`
 --
 ALTER TABLE `tb_pegawai`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `nik` (`nik`);
 
 --
 -- Indexes for table `tb_presensi_ujian`
@@ -525,7 +539,7 @@ ALTER TABLE `tb_matapelajaran`
 -- AUTO_INCREMENT for table `tb_pegawai`
 --
 ALTER TABLE `tb_pegawai`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_presensi_ujian`
